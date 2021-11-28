2c57b1b7c07487e5f88aa9bd81451fcb5769de9a
Ibrahyma <i.missoma@gmail.com>
2021-11-29 00:29:03 +0100

    Sockets with console gameplay

--- clientListener.py
+++ clientListener.py
@@ -7,32 +7,61 @@ class ClientListener(threading.Thread):
         self.socket = socket
         self.address = address
         self.listening = True
-        self.username = "No username"
 
     def run(self):
         while self.listening:
+            self.server.echo(f"\n\nModèle du mot : {self.server.modele_du_mot}\n")
+            self.server.echo(f"Lettres utilisées : {str(self.server.lettres_proposees)}\n")
+
             data = ""
             try:
-                data = self.socket.recv(1024).decode('UTF-8')
+                data: str = self.socket.recv(1024).decode('UTF-8')
+                letter = data[0].lower()
+
+                if letter == "":
+                    raise Exception()
+                if letter in self.server.mot_secret:
+                    if not letter in self.server.lettres_proposees:	
+                        self.server.points += self.server.mot_secret.count(letter)
+                        self.server.echo("\n\nExact. Vous avez " + str(self.server.points) + " points.\n")
+
+                        for x in [m.start() for m in re.finditer(letter, self.server.mot_secret)]:
+                            modele = self.server.str_remove_spaces(self.server.modele_du_mot)
+                            nouveau_modele = self.server.replacer(modele, letter, x)
+                            self.server.modele_du_mot = self.server.str_add_spaces_between_chars(nouveau_modele)
+                            self.server.echo(f"\n\nModèle du mot : {self.server.modele_du_mot}")
+
+                        if self.server.points == len(self.server.mot_secret):
+                            self.server.echo("\nGG !!!")
+                            exit(0)
+                    else:
+                        self.server.echo("Cette lettre a déjà été proposée...")
+                else:
+                    self.server.essais_restants -= 1
+                    if self.server.essais_restants == 0:
+                        self.server.echo("Perdu. Le mot était '" + self.server.mot_secret + "'.")
+                        exit(0)
+
+                    self.server.echo(f"Faux. Il reste {str(self.server.essais_restants)} essais.")
+
+                if not letter in self.server.lettres_proposees:
+                    self.server.lettres_proposees.append(letter)
+
             except socket.error:
                 print("Unable to receive data")
+            except Exception:
+                print()
             self.handle_msg(data)
             time.sleep(0.1)
-        print("Ending client thread for", self.address)
 
     def quit(self):
         self.listening = False
         self.socket.close()
         self.server.remove_socket(self.socket)
-        self.server.echo("{0} has quit\n".format(self.username))
 
     def handle_msg(self, data):
         print(self.address, "sent :", data)
-        username_result = re.search('^USERNAME (.*)$', data)
-        if username_result:
-            self.username = username_result.group(1)
-            self.server.echo("{0} has joined.\n".format(self.username))
-        elif data == "QUIT" or data == "":
+        if data == "QUIT" or data == "":
             self.quit()
         else:
             self.server.echo(data)
