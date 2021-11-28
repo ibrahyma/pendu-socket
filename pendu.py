959c63306b8d46e9eeec2e78848a064d0a177b89
Ibrahyma <i.missoma@gmail.com>
2021-11-28 20:52:40 +0100

    Clean code

--- pendu.py
+++ pendu.py
@@ -1,59 +1,96 @@
 import re, random
+from tkinter import *
+from tkinter import messagebox
+from pygame.locals import *
 
-mots = ["pizza", "salade", "donner", "bonjour", "calme", "paisible", "pluie", "mer", "eau", "tomate", "oignon"]
-mot_secret = mots[random.randrange(0, len(mots))]
-essais_restants = 20
-points = 0
-modele_du_mot = ("_" * len(mot_secret))
-lettres_proposees = []
+def str_add_spaces_between_chars(str: str):
+	return " ".join(str)
 
+def str_remove_spaces(str: str):
+	return str.replace(' ', '')
 
-def replacer(s, newstring, index, nofail=False):
-    if not nofail and index not in range(len(s)):
-        raise ValueError("index outside given string")
+class Pendu:
+	def __init__(self, window) -> None:
+		# self.mots = ["pizza", "salade", "donner", "bonjour", "calme", "paisible", "pluie", "mer", "eau", "tomate", "oignon"]
+		self.mots = ["ibrahyma"]
+		self.mot_secret = self.mots[random.randrange(0, len(self.mots))]
+		self.essais_restants = 13
+		self.points = 0
+		self.modele_du_mot = str_add_spaces_between_chars("_" * len(self.mot_secret))
+		self.lettres_proposees = []
 
-    if index < 0:
-        return newstring + s
-    if index > len(s):
-        return s + newstring
+		self.txtvar_label_motsecret = StringVar()
+		self.txtvar_label_essais = StringVar()
+		self.txtvar_textfield = StringVar()
+		self.txtvar_label_motsecret.set(self.modele_du_mot)
+		self.txtvar_label_essais.set(f"{self.essais_restants} essais avant la pendaison")
+		self.txtvar_textfield.set('')
+		self.label_motsecret = Label(window, textvariable = self.txtvar_label_motsecret, font = ("Helvetica", 32))
+		self.label_essais = Label(window, textvariable = self.txtvar_label_essais, fg = 'red', font = ("Helvetica", 16))
+		self.textfield = Entry(window, textvariable = self.txtvar_textfield)
+		self.button = Button(window, text = "Essayer")
+		self.button.bind('<Button-1>', self.submit_hander)
+		
+		self.label_essais.place(x = 25, y = 25)
+		self.label_motsecret.place(x = 25, y = 100)
+		self.textfield.place(x = 25, y = 150)
+		self.button.place(x = 25, y = 200)
 
-    return s[:index] + newstring + s[index + 1:]
+	def replacer(self, s, newstring, index, nofail=False):
+		if not nofail and index not in range(len(s)):
+			raise ValueError("index outside given string")
+		if index < 0:
+			return newstring + s
+		if index > len(s):
+			return s + newstring
 
-while True:
-	print("\n\nModèle du mot : " + modele_du_mot)
-	print("Lettres utilisées : " + str(lettres_proposees))	
+		return s[:index] + newstring + s[index + 1:]
 
-	lettre = input("Proposez une lettre: ")
-	if len(lettre) > 1:
-		if lettre == mot_secret:
-			print("Tu as gagné, le mot était '" + mot_secret + "'.")
-			break
+	def is_already_tried(self, lettre: str) -> bool:
+		return lettre in self.lettres_proposees
+
+	def submit_hander(self, event):
+		lettre = self.textfield.get()[0].lower()
+		self.txtvar_textfield.set('')
+		isAlreadyTried = self.is_already_tried(lettre)
+
+		if lettre in self.mot_secret:
+			if not isAlreadyTried:	
+				self.points += self.mot_secret.count(lettre)
+
+				for x in [m.start() for m in re.finditer(lettre, self.mot_secret)]:
+					modele = str_remove_spaces(self.modele_du_mot)
+					nouveau_modele = self.replacer(modele, lettre, x)
+					self.modele_du_mot = str_add_spaces_between_chars(nouveau_modele)
+
+				self.txtvar_label_motsecret.set(self.modele_du_mot)
+
+				if self.points == len(self.mot_secret):
+					messagebox.showinfo(title = "GG", message = "GG")
+					exit(0)
+
+			else:
+				messagebox.showinfo(title = "Attention", message = "Cette lettre a déjà été proposée...")
+
 		else:
-			print("Erreur: le mot ne correspond pas.")
-			continue
-
-	if lettre in mot_secret:
-		if not lettre in lettres_proposees:	
-			points = points + mot_secret.count(lettre)
-			print("Exact. Vous avez " + str(points) + " points.")
-
-			for x in [m.start() for m in re.finditer(lettre, mot_secret)]:
-				modele_du_mot = replacer(modele_du_mot, lettre, x)
-
-			if points == len(mot_secret):
-				print("Tu as gagné")
-				break
-		else:
-			print("Cette lettre a déjà été proposée...")
-
-	else:
-		essais_restants = essais_restants - 1
-		if essais_restants == 0:
-			print ("Tu as perdu. Le mot était '" + mot_secret + "'.")
-			break
-		print("Faux. Il vous reste " + str(essais_restants) + " essais.")
-
-	if not lettre in lettres_proposees: lettres_proposees.append(lettre)
+			if not isAlreadyTried:
+				self.essais_restants -= 1
+				self.txtvar_label_essais.set(f"{self.essais_restants} essais avant la pendaison")
+				messagebox.showinfo(title = "Ooops", message = "Raté")
 
 
+				if self.essais_restants == 0:
+					messagebox.showinfo(title = "Bien essayé", message = "Tu as perdu. Le mot était '" + self.mot_secret + "'.")
+					exit(0)
+			else:
+				messagebox.showinfo(title = "Attention", message = "Cette lettre a déjà été proposée...")
 
+		if not lettre in self.lettres_proposees: self.lettres_proposees.append(lettre)
+
+
+if __name__ == "__main__":
+	window = Tk()
+	pendu = Pendu(window)
+	window.title('Pendu')
+	window.geometry('400x300')
+	window.mainloop()
